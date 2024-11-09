76110a0b3bee2dfcc541b7928d990a1a830eeb66
chaeleen <cl27au@gmail.com>
2024-11-09 13:52:41 +0900

    front 수정

--- frontend/src/pages/MovieDetail/MovieDetail.jsx
+++ frontend/src/pages/MovieDetail/MovieDetail.jsx
@@ -1,5 +1,34 @@
 import React from "react";
 
+import "./MovieDetail.css";
+
 export default function MovieDetail() {
-  return <div>MovieDetail</div>;
+  const movieDetail = {
+    movie_id: "1",
+    movie_name: "명량",
+    release_date: "2014-07-30",
+    take: "135762515310",
+    attendance: "17616371",
+    screeyarnn: "1587",
+    screenings: "188730",
+    representative_nationality: "한국",
+    nationality: "한국",
+    distributor: "(주)씨제이이엔엠",
+  };
+
+  return (
+    <div className="MovieDetail">
+      <div className="poster">
+        <img
+          className="posterImage"
+          src={process.env.PUBLIC_URL + `/images/test.jpg`}
+          alt="poster"
+        />
+      </div>
+
+      <div className="infoContainer">
+        <h1>{movieDetail.movie_name}</h1>
+      </div>
+    </div>
+  );
 }
